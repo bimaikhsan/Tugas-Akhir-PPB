5512b536c1103fc042bb3403e091032a86a6d419
bimaikhsan <bimaikhsan.bi@gmail.com>
2021-12-14 22:03:29 +0700

    edit register dan login

--- app/src/main/java/tugasakhirppb/com/MainActivity.java
+++ app/src/main/java/tugasakhirppb/com/MainActivity.java
@@ -1,12 +1,22 @@
 package tugasakhirppb.com;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     Button login, regis;
@@ -17,10 +27,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         login=findViewById(R.id.login);
         regis=findViewById(R.id.daftar);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myref = database.getReference("user");
+
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Login();
+                Query query =myref.orderByChild("username").equalTo(username.getText().toString());
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()){
+                            for (DataSnapshot user : snapshot.getChildren()) {
+                                String getPassword = user.child("password").getValue(String.class);
+                                if (password.getText().toString().equals(getPassword)) {
+                                    Toast.makeText(MainActivity.this,"Berhasil login",Toast.LENGTH_SHORT).show();
+                                    Login();
+                                }else{
+                                    password.setError("Password salah");
+                                }
+                            }
+                        }else{
+                            username.setError("username tidak ditemukan");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
         regis.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/tugasakhirppb/com/daftar.java
+++ app/src/main/java/tugasakhirppb/com/daftar.java
@@ -6,23 +6,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class daftar extends AppCompatActivity {
-    Button daftar;
+    ImageButton daftar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
         daftar=findViewById(R.id.daftar);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myref = database.getReference("user");
+
+        EditText username = findViewById(R.id.username);
+        EditText email = findViewById(R.id.email);
+        EditText nohp = findViewById(R.id.nomorHP);
+        EditText pass = findViewById(R.id.password);
+
         daftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                masuk();
+                if (username.getText().toString().length() == 0){
+                    username.setError("username tidak boleh kosong");
+                }else if(email.getText().toString().length() == 0){
+                    email.setError("Email tidak boleh kosong");
+                }else if(nohp.getText().toString().length() == 0){
+                    nohp.setError("nohp tidak boleh kosong");
+                }else if(pass.getText().toString().length() == 0){
+                    pass.setError("password tidak boleh kosong");
+                }else{
+                    myref.child(username.getText().toString()).child("username").setValue(username.getText().toString());
+                    myref.child(username.getText().toString()).child("email").setValue(email.getText().toString());
+                    myref.child(username.getText().toString()).child("nomor").setValue(nohp.getText().toString());
+                    myref.child(username.getText().toString()).child("password").setValue(pass.getText().toString());
+                    masuk();
+                }
             }
         });
     }
     public void masuk (){
-        Intent intent = new Intent(this, beranda.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
